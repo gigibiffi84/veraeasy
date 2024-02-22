800209626e113cb788c58c419120a0217b17d901
Luigi Bifulco <bifulcoluigi@gmail.com>
2024-02-22 15:59:06 +0100

    jsx

--- Frontend/TranslateApp/jsx/src/App.js
+++ Frontend/TranslateApp/jsx/src/App.js
@@ -2,10 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const sum = 1 + 2;
+  const message = `Hi There ${sum}`;
+  const style = { border: '3px solid red' };
   return (
-    <div className="App">
-      <h1>Hi There! </h1>
-    </div>
+
+    <section className={"layout"}>
+      <div className={"header"}>  <h1>{message}</h1>
+        <input width={"100%"} type={"number"} minLength={5} maxLength={10} style={style}></input></div>
+      <div className={"leftSide"}>2</div>
+      <div className={"body"}>3</div>
+      <div className={"rightSide"}>4</div>
+      <div className={"footer"}>5</div>
+    </section>
+
   );
 }
 
