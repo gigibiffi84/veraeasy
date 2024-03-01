3f75ca2b330633b0a228745792964e29b26b0311
Luigi Bifulco <bifulcoluigi@gmail.com>
2024-03-01 10:47:46 +0100

    add personid migrations

---
+++ Src/Veraeasy/ContactVerification/Data/Database/Migrations/20240301094250_AddPersonId.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Veraeasy.ContactVerification.Data.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPersonId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PersonId",
+                schema: "ContactVerification",
+                table: "ContactVerification",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PersonId",
+                schema: "ContactVerification",
+                table: "ContactVerification");
+        }
+    }
+}
